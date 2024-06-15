9d83b64a2da36bb5676e74f891a568ad598ccd84
RajGupta-0904 <rajgupta1.0904@gmail.com>
2024-06-15 13:17:43 +0530

    final

--- routes.js
+++ routes.js
@@ -1,1 +1,9 @@
-console.log("routes")
+const express = require('express');
+const userRoutes = require('./routes/userRoutes');
+
+const router = express.Router();
+
+//Mounting routes
+router.use(userRoutes);
+
+module.exports = router;
